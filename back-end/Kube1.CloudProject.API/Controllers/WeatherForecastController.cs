9dd28fc042b7e9a7327f7395908a39642cce1c6f
Minh Tân <minhtantanhiep@gmail.com>
2020-12-12 09:50:50 +0700

    []

--- back-end/Kube1.CloudProject.API/Controllers/WeatherForecastController.cs
+++ back-end/Kube1.CloudProject.API/Controllers/WeatherForecastController.cs
@@ -31,8 +31,7 @@ namespace Kube1.CloudProject.API.Controllers
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
                 Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+            }).ToArray();
         }
     }
 }
